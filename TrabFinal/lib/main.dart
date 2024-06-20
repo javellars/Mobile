e11b5678a59e5a8dbcb2c0300bd16c055da59bd2
Jullia <robas.jullia@gmail.com>
2024-06-19 23:52:17 -0300

    Configs iniciais de app no firebase (add firebase_core)

--- TrabFinal/lib/main.dart
+++ TrabFinal/lib/main.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:entrega1_livraria/view/inicio_screen.dart';
 import 'package:entrega1_livraria/bloc/profiler_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+void main() { 
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCqySIwOtGFNKPJtcWaj__irz6SrD4TxlM", 
+      authDomain: "bookshop-cae03.firebaseapp.com",
+      databaseURL: "https://bookshop-cae03-default-rtdb.firebaseio.com",
+      storageBucket: "bookshop-cae03.appspot.com",
+      appId:"1:293707449516:web:7286fd20a3657bf85c0b09", 
+      messagingSenderId: "293707449516", 
+      projectId: "bookshop-cae03"
+      )
+    );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
